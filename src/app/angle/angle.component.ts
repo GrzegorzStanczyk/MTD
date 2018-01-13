d6ef030332a3c2d7da60d3d2b71df0ba9e3294d3
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-01-13 16:04:10 +0100

    Added g-force canvas board

--- src/app/angle/angle.component.ts
+++ src/app/angle/angle.component.ts
@@ -18,14 +18,17 @@ import 'rxjs/add/operator/map';
 })
 export class AngleComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('telemetryCanvas') telemetryCanvas;
+  @ViewChild('accelerationCanvas') accelerationCanvas;
   // public angle$: Observable<number>;
   public angle: number;
   public acceleration$: Observable<any>;
 
-  private canvas: HTMLCanvasElement;
+  private telemetry: HTMLCanvasElement;
+  private acceleration: HTMLCanvasElement;
   // private canvas: HTMLCanvasElement = this.telemetryCanvas.nativeElement;
-  // private ctx: CanvasRenderingContext2D = this.canvas.getContext('2d');
-  private ctx: CanvasRenderingContext2D;
+  // private telemetryCTX: CanvasRenderingContext2D = this.telemetry.getContext('2d');
+  private telemetryCTX: CanvasRenderingContext2D;
+  private accelerationCTX: CanvasRenderingContext2D;
   private hostSize: {width: number, height: number};
   private leanAngleSize: {x: number, y: number};
   private radius: number;
@@ -43,6 +46,7 @@ export class AngleComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit() {
     this.setCanvasSize();
+    this.drawGForce();
     this.setGradient();
     // this.angle$ = this.eventListenerService.angle$.map(val => Math.round(val));
     this.eventListenerService.angle$.subscribe(val => this.angle = Math.round(val));
@@ -51,14 +55,18 @@ export class AngleComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   setCanvasSize(): void {
-    this.canvas = this.telemetryCanvas.nativeElement;
-    this.ctx = this.canvas.getContext('2d');
+    this.telemetry = this.telemetryCanvas.nativeElement;
+    this.telemetryCTX = this.telemetry.getContext('2d');
+    this.acceleration = this.accelerationCanvas.nativeElement;
+    this.accelerationCTX = this.acceleration.getContext('2d');
     this.hostSize = {
       width: this.el.nativeElement.clientWidth,
       height: this.el.nativeElement.clientHeight
     };
-    this.canvas.width = this.hostSize.width;
-    this.canvas.height = this.hostSize.height;
+    this.telemetry.width = this.hostSize.width;
+    this.telemetry.height = this.hostSize.height;
+    this.acceleration.width = this.hostSize.width;
+    this.acceleration.height = this.hostSize.height;
     this.leanAngleSize = {
       x: this.hostSize.width / 2,
       y: this.hostSize.height / 2
@@ -67,14 +75,14 @@ export class AngleComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   setGradient(): void {
-    this.grd = this.ctx.createLinearGradient(0, 0, this.hostSize.width, 0);
+    this.grd = this.telemetryCTX.createLinearGradient(0, 0, this.hostSize.width, 0);
     this.grd.addColorStop(0, 'red');
     this.grd.addColorStop(0.2, 'orange');
     this.grd.addColorStop(0.4, 'green');
     this.grd.addColorStop(0.6, 'green');
     this.grd.addColorStop(0.8, 'orange');
     this.grd.addColorStop(1, 'red');
-    this.ctx.strokeStyle = this.grd;
+    this.telemetryCTX.strokeStyle = this.grd;
   }
 
   degreesToRadians (degrees): number {
@@ -82,20 +90,41 @@ export class AngleComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   drawLeanAngle(): void {
-    this.ctx.beginPath();
+    this.telemetryCTX.beginPath();
     if (this.angle > 0) {
-      this.ctx.arc(this.leanAngleSize.x, this.leanAngleSize.y, this.radius,
+      this.telemetryCTX.arc(this.leanAngleSize.x, this.leanAngleSize.y, this.radius,
         this.startingPoint, this.degreesToRadians(this.angle) + this.startingPoint, false);
     } else {
-      this.ctx.arc(this.leanAngleSize.x, this.leanAngleSize.y, this.radius,
+      this.telemetryCTX.arc(this.leanAngleSize.x, this.leanAngleSize.y, this.radius,
         this.startingPoint, this.degreesToRadians(this.angle) - Math.PI / 2, true);
     }
-    this.ctx.lineWidth = this.lineWidth;
-    this.ctx.stroke();
+    this.telemetryCTX.lineWidth = this.lineWidth;
+    this.telemetryCTX.stroke();
+  }
+
+  drawGForce() {
+    this.accelerationCTX.beginPath();
+    this.accelerationCTX.moveTo(this.lineWidth, this.acceleration.height / 2);
+    this.accelerationCTX.lineTo(this.acceleration.width - this.lineWidth, this.acceleration.height / 2);
+    this.accelerationCTX.stroke();
+
+    this.accelerationCTX.beginPath();
+    this.accelerationCTX.moveTo(this.acceleration.width / 2, this.lineWidth);
+    this.accelerationCTX.lineTo(this.acceleration.width / 2, this.acceleration.height - this.lineWidth);
+    this.accelerationCTX.stroke();
+
+    this.accelerationCTX.beginPath();
+    this.accelerationCTX.arc(this.acceleration.width / 2, this.acceleration.height / 2, ((this.acceleration.width / 2) -  this.lineWidth) * 1 / 3,
+      0, Math.PI * 2, true);
+    this.accelerationCTX.arc(this.acceleration.width / 2, this.acceleration.height / 2, ((this.acceleration.width / 2) -  this.lineWidth) * 2 / 3,
+      0, Math.PI * 2, true);
+    this.accelerationCTX.arc(this.acceleration.width / 2, this.acceleration.height / 2, (this.acceleration.width / 2) -  this.lineWidth,
+      0, Math.PI * 2, true);
+    this.accelerationCTX.stroke();
   }
 
   clearCanvas(): void {
-    this.ctx.clearRect(0, 0, this.hostSize.width, this.hostSize.height);
+    this.telemetryCTX.clearRect(0, 0, this.hostSize.width, this.hostSize.height);
   }
 
   loop(): void {
